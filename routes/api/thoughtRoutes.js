3c2fd2d3aae94e85f980dc5a162a7cdce265a14d
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-31 18:10:33 -0500

    created thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(createReaction);
+
+// /api/thoughts/:thoughtsId/reactions/:reactionsId
+router.route('/:thoughtsId/reactions/:reactionsId')
+    .delete(deleteReaction);
