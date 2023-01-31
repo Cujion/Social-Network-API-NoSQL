c1bd1ca01aeb16c0a8333c906a17d985fc90406c
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-31 18:03:23 -0500

    added userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    deleteUsersThoughts, //Bonus
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/thoughts/:thoughtsId
+router.route('/:userId/thoughts/:thoughtsId')
+    .delete(deleteUsersThoughts);
+
+// /api/users/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendsId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
