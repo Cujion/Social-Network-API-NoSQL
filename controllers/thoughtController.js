16367e4b4372d21c420e5c6b11f397bc09a9a102
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-31 20:17:29 -0500

    created updateThought method

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     // Get a Single Thought by ID
     getSingleThought(req, res) {
-        Thought.findOne({ _id: req.params.userId })
+        Thought.findOne({ _id: req.params.thoughtId })
         .select('-__v')
         .then((thought) =>
             !thought
@@ -41,7 +41,17 @@ module.exports = {
     },
     // Update a Thought by ID
     updateThought(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((thought) =>
+            !thought
+                ? res.status(404).json({ message: 'No Thoughts with that ID :(' })
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
     },
     // Delete a Thought by ID
     deleteThought(req, res) {
