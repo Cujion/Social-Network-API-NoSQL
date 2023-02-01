daa60a2c2d4e3d3551061e80fa1433dabf48f9df
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-31 21:37:32 -0500

    removed utils with helper.js file since using dayjs for date formatting

--- utils/helpers.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    format_date: (date) => {
-      return `${new Date(date).getMonth()} ${new Date(date).getDate()}, ${
-        new Date(date).getFullYear()} at ${new Date(date).getHours}${new Date(date).getMinutes}`;
-    },
-  };
-  
